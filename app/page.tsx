8ee87141acf816911c338f1edb6a27c0508b303e
Raunak kumar rao <sraunak204@gmail.com>
2026-09-06 14:00:03 +0530

    feat: add resume access

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import TeamProfiles from "@/components/TeamProfiles";
-import Skills from "@/components/Skills";
+import Team from "@/components/Team";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
-      <TeamProfiles />
+      <Team />
     </main>
   );
 }
